396ede202dec2f5709682cf3f4d44a5934529d9a
rcung000 <cung.richard0@gmail.com>
2019-12-23 23:14:01 -0500

    added tree

--- LHnetwork.py
+++ LHnetwork.py
@@ -1,34 +1,53 @@
 import random
 
 tuplesize = 3
-MSet = 4
-J1 = []
-J2 = []
-J3 = []
-J4 = []
+H_tuple1 = []
+H_tuple2 = []
+H_tuple3 = []
+H_tuple4 = []
 pos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 poscount = 0
 for x in range(0, tuplesize, 1):
-    J1.append(0)
-    J1[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(J1[x])
+    H_tuple1.append(0)
+    H_tuple1[x] = pos[random.randint(0, 11 - poscount)]
+    pos.remove(H_tuple1[x])
     poscount += 1
 
-    J2.append(0)
-    J2[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(J2[x])
+    H_tuple2.append(0)
+    H_tuple2[x] = pos[random.randint(0, 11 - poscount)]
+    pos.remove(H_tuple2[x])
     poscount += 1
 
-    J3.append(0)
-    J3[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(J3[x])
+    H_tuple3.append(0)
+    H_tuple3[x] = pos[random.randint(0, 11 - poscount)]
+    pos.remove(H_tuple3[x])
     poscount += 1
 
-    J4.append(0)
-    J4[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(J4[x])
+    H_tuple4.append(0)
+    H_tuple4[x] = pos[random.randint(0, 11 - poscount)]
+    pos.remove(H_tuple4[x])
     poscount += 1
-print(J1)
-print(J2)
-print(J3)
-print(J4)
+
+def binarytree(tuplearr):
+    if(tuplearr[0] == 0):
+        if(tuplearr[1] == 0):
+            if(tuplearr[2] == 0):
+                return '000'
+            else:
+                return '001'
+        else:
+            if(tuplearr[2] == 0):
+                return '010'
+            else:
+                return '011'
+    else:
+        if(tuplearr[1] == 0):
+            if(tuplearr[2] == 0):
+                return '100'
+            else:
+                return '101'
+        else:
+            if(tuplearr[2] == 0):
+                return '110'
+            else:
+                return '111'
