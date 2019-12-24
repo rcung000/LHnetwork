c4771f419a39618392085e72d295c0b75a27bedb
rcung000 <cung.richard0@gmail.com>
2019-12-24 11:47:04 -0500

    refined tupleassign process

--- LHnetwork.py
+++ LHnetwork.py
@@ -1,32 +1,34 @@
 import random
 
 tuplesize = 3
-H_tuple1 = []
-H_tuple2 = []
-H_tuple3 = []
-H_tuple4 = []
-pos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-poscount = 0
-for x in range(0, tuplesize, 1):
-    H_tuple1.append(0)
-    H_tuple1[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(H_tuple1[x])
-    poscount += 1
 
-    H_tuple2.append(0)
-    H_tuple2[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(H_tuple2[x])
-    poscount += 1
+def tupleassign(tuplesize):
+    """assigns a set randomly generated tuple position"""
+    pos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    poscount = 0
+    cookie = [[], [], [], []]
+    for x in range(0, tuplesize, 1):
+        cookie[0].append(0)
+        cookie[0][x] = pos[random.randint(0, 11 - poscount)]
+        pos.remove(cookie[0][x])
+        poscount += 1
 
-    H_tuple3.append(0)
-    H_tuple3[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(H_tuple3[x])
-    poscount += 1
+        cookie[1].append(0)
+        cookie[1][x] = pos[random.randint(0, 11 - poscount)]
+        pos.remove(cookie[1][x])
+        poscount += 1
 
-    H_tuple4.append(0)
-    H_tuple4[x] = pos[random.randint(0, 11 - poscount)]
-    pos.remove(H_tuple4[x])
-    poscount += 1
+        cookie[2].append(0)
+        cookie[2][x] = pos[random.randint(0, 11 - poscount)]
+        pos.remove(cookie[2][x])
+        poscount += 1
+
+        cookie[3].append(0)
+        cookie[3][x] = pos[random.randint(0, 11 - poscount)]
+        pos.remove(cookie[3][x])
+        poscount += 1
+    return cookie
+
 
 def binarytree(tuplearr):
     """used to read and determine values"""
