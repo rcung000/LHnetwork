207d7506e7d103fab39df32bcf2ca960d93bf3b8
rcung000 <cung.richard0@gmail.com>
2019-12-24 19:19:50 -0500

    cleaned up file and added comments

--- LHnetwork.py
+++ LHnetwork.py
@@ -4,7 +4,7 @@ tuplesize = 3
 
 def tupleassign(tuplesize):
     """assigns a set randomly generated tuple position"""
-    pos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    pos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     poscount = 0
     cookie = [[], [], [], []]
     for x in range(0, tuplesize, 1):
@@ -35,22 +35,82 @@ def binarytree(tuplearr):
     if(tuplearr[0] == 0 or tuplearr[0] == '0'):
         if(tuplearr[1] == 0 or tuplearr[1] == '0'):
             if(tuplearr[2] == 0 or tuplearr[2] == '0'):
-                return '000'
+                return 0
             else:
-                return '001'
+                return 1
         else:
             if(tuplearr[2] == 0 or tuplearr[2] == '0'):
-                return '010'
+                return 2
             else:
-                return '011'
+                return 3
     else:
         if(tuplearr[1] == 0 or tuplearr[1] == '0'):
             if(tuplearr[2] == 0 or tuplearr[2] == '0'):
-                return '100'
+                return 4
             else:
-                return '101'
+                return 5
         else:
             if(tuplearr[2] == 0 or tuplearr[2] == '0'):
-                return '110'
+                return 6
             else:
-                return '111'
+                return 7
+
+
+# RANDOMLY GENERATED ONLY ONCE (J sets)
+placements = tupleassign(tuplesize)
+print(placements)
+# Corresponding J sets
+L_1 = []
+L_2 = []
+L_3 = []
+L_4 = []
+# TRAINING MEMORY
+H_tuple1 = [0, 0, 0, 0, 0, 0, 0, 0]
+H_tuple2 = [0, 0, 0, 0, 0, 0, 0, 0]
+H_tuple3 = [0, 0, 0, 0, 0, 0, 0, 0]
+H_tuple4 = [0, 0, 0, 0, 0, 0, 0, 0]
+L_tuple1 = [0, 0, 0, 0, 0, 0, 0, 0]
+L_tuple2 = [0, 0, 0, 0, 0, 0, 0, 0]
+L_tuple3 = [0, 0, 0, 0, 0, 0, 0, 0]
+L_tuple4 = [0, 0, 0, 0, 0, 0, 0, 0]
+
+# Begin reading from Datasets
+L_dataset = "L_images.txt"
+H_dataset = "H_images.txt"
+H_open = open(H_dataset)
+for a in range(100, 299, 1):
+    H_data = H_open.readline()
+    #Strip off [ , ] and isolate
+    step0 = H_data.split('[')
+    step1 = step0[1]
+    step2 = step1.split(']')
+    step3 = step2[0]
+    step4 = step3.split(', ')
+    H_arr = []
+    # Corresponding J sets
+    H_1 = []
+    H_2 = []
+    H_3 = []
+    H_4 = []
+    for x in range(0, 12, 1):
+        H_arr.append(step4[x])
+    print(H_arr)
+    #Split into tuples
+    for a in range(0, tuplesize, 1) :
+        H_1.append(H_arr[placements[0][a]])
+        H_2.append(H_arr[placements[1][a]])
+        H_3.append(H_arr[placements[2][a]])
+        H_4.append(H_arr[placements[3][a]])
+    print (H_1)
+    print (H_2)
+    print (H_3)
+    print (H_4)
+    #Count into memory
+    H_tuple1[binarytree(H_1)] += 1
+    H_tuple2[binarytree(H_2)] += 1
+    H_tuple3[binarytree(H_3)] += 1
+    H_tuple4[binarytree(H_4)] += 1
+    print (H_tuple1)
+    print (H_tuple2)
+    print (H_tuple3)
+    print (H_tuple4)
